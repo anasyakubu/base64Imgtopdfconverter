2ba318a190f2656e21a4f2453a3f0e16f89f9e05
ImArnoldDCODES <danielsarnold010@gmail.com>
2024-05-10 18:18:09 +0100

    refactored

--- server.cjs
+++ server.cjs
@@ -1,27 +1,14 @@
-const express = require('express');
-const fileUpload = require('express-fileupload');
 const fs = require('fs');
-const PDFDocument = require('pdfkit');
-const crypto = require('crypto');
 const sharp = require('sharp');
+const express = require('express');
+const PDFDocument = require('pdfkit');
+const fileUpload = require('express-fileupload');
 
 const app = express();
 const port = 3000;
 
 app.use(fileUpload());
 app.use(express.static('public'));
-
-function generateHash(data) {
-  const hash = crypto.createHash('sha256');
-  hash.update(data);
-  return hash.digest('hex');
-}
-
-function generateFilename(base64String) {
-  const hash = generateHash(base64String);
-  return hash + '.pdf';
-}
-
 
 app.post('/upload', async (req, res) => {
   try {
@@ -30,25 +17,22 @@ app.post('/upload', async (req, res) => {
     }
 
     const imageData = Buffer.from(req.files.image.data);
-    const base64String = imageData.toString('base64');
     const resizedImage = await sharp(imageData).resize({ width: 500 }).toBuffer();
+    const file = imageData.toString('base64').substring(0, 2) + '.pdf'
+    const writeStream = fs.createWriteStream(file);
 
     const doc = new PDFDocument();
-    const filename = generateFilename(base64String);
-    const writeStream = fs.createWriteStream(filename);
-
     doc.image(resizedImage, 50, 50);
-
     doc.pipe(writeStream);
     doc.end();
 
     writeStream.on('finish', () => {
-      res.download(filename, 'output.pdf', (err) => {
+      res.download(file, 'output.pdf', (err) => {
         if (err) {
           console.error('Error downloading file:', err);
           return res.status(500).send('Error downloading file.');
         }
-        fs.unlinkSync(filename);
+        fs.unlinkSync(file);
       });
     });
 
